80c66fd9dde5fbb309554673e2faa28b516f8cef
Ann <aldestajo@up.edu.ph>
2023-05-28 17:18:24 +0800

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import { Card, CardGroup, Col, Container, Image, Row } from "react-bootstrap";
 import AppNavbar from "../components/AppNavbar";
-import image1 from "../static/images/blog_thumbnail.png";
+import image1 from "../static/images/oasis-pic-1.jpg";
+import image2 from "../static/images/oasis-pic-2.jpg";
+import image3 from "../static/images/oasis-pic-3.jpg";
 
 const About = () => {
     return ( 
@@ -12,42 +14,58 @@ const About = () => {
                     <Col>
                         <Container fluid>
                             <Container className="d-flex flex-row p-4 ">
-                                <span>
-                                    <h4>who we are</h4>
-                                    <p>oasis is a safe space for women in colleges and universities to come together—an oasis for their thoughts, experiences, and sufferings to be validated and given support.</p>
+                                <span className="about-paragraph">
+                                    <h2 className="about-label">who we are</h2>
+                                    <p className="about-title-text"><strong>oasis</strong> is a nurturing and empowering online community that provides a safe space for women to connect, share, and support one another. Within oasis, women find solace and validation for their thoughts, experiences, and challenges, knowing they are heard and understood. By fostering a sense of belonging, this platform aims to create a network of support that encourages personal growth, healing, and freedom of expression.</p>
                                 </span>
-                                <span>
-                                    <Image className="img-fluid " src={image1}/>
-                                </span>
+                      
+                                    <Image className="about-img" src={image1}/>
+                   
                             </Container>
 
                             <Container className="d-flex flex-column p-4 ">
-                                <p>oasis will provide an avenue for women to</p>
-                                <Row className={"flex-nowrap overflow-scroll   "}>
-                                    <Card><Card.Body>sdsdsdsds</Card.Body></Card>
-                                    <Card><Card.Body>sdsdsdsds</Card.Body></Card>
-                                    <Card><Card.Body>sdsdsdsds</Card.Body></Card>
-                                    <Card><Card.Body>sdsdsdsds</Card.Body></Card>
+                                <Row className={"flex-nowrap overflow-auto "} id="about-row">
+                                    <Card className="about-us">
+                                        <Card.Body className="about-text-3">
+                                            <Card.Title className="about-text">feel safe</Card.Title>
+                                            <Card.Subtitle>when sharing stories</Card.Subtitle>
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="about-us">
+                                        <Card.Body className="about-text-3">
+                                            <Card.Title className="about-text">seek refuge</Card.Title>
+                                            <Card.Subtitle>from psychiatrists</Card.Subtitle>
+                                        </Card.Body>
+                                    </Card>
+
+                                    <Card className="about-us">
+                                        <Card.Body className="about-text-3">
+                                            <Card.Title className="about-text">find solace</Card.Title>
+                                            <Card.Subtitle>in a community</Card.Subtitle>
+                                        </Card.Body>
+                                    </Card>
+                                  
                                 </Row>
                             </Container>
 
                             <Container className="d-flex flex-row-reverse p-4 ">
-                                <span>
-                                    <h4>mission</h4>
+                                <span className="about-paragraph">
+                                    <h4 className="about-label"> mission</h4>
                                     <p>To provide a safe haven for women and young girls in schools and universities to seek refuge in, where they can form a community with shared experiences, all while being provided with proper access to help with their mental, emotional, and psychological needs.</p>
                                 </span>
                                 <span>
-                                    <Image className="img-fluid " src={image1}/>
+                                    <Image className="about-img" src={image2}/>
                                 </span>
                             </Container>
 
                             <Container className="d-flex flex-row p-4">
-                                <span>
-                                    <h4>vision</h4>
+                                <span className="about-paragraph">
+                                    <h4 className="about-label">vision</h4>
                                     <p>To be a catalyst of change in pushing for a VAW-free society and act as a frontline service provider for struggling female students who experienced forms of abuse in universities and schools nationwide.</p>
                                 </span>
                                 <span>
-                                    <Image className="img-fluid" src={image1}/>
+                                    <Image className="about-img" src={image3}/>
                                 </span>
                             </Container>
                         </Container>
